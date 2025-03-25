6cd9bcebd29b1b67c2cba9376bf6ca2831381b2f
Md-Mujahid92 <120643682+Md-Mujahid92@users.noreply.github.com>
2025-03-25 13:32:01 +0530

    update some changes

--- src/views/Blog/config/blogData.js
+++ src/views/Blog/config/blogData.js
@@ -19,7 +19,7 @@ export const blogData = [
     content:
       "We understand how important work spaces are. And so, we didn’t build the average co-working space. We designed a space where you would love to return everyday for work, where creativity will thrive, ideas and innovations would grow and teams will connect and collaborate with another to help achieve the goal. Whether you are an entrepreneur, freelancer or a small business, you are welcome to enjoy your office space. Welcome to the future of workspaces.",
     author: "Eccosphere",
-    date: "March 2, 2025",
+    date: "March 2, 2021",
   },
   {
     id: 2,
@@ -30,7 +30,7 @@ export const blogData = [
     content:
       "We are home to a vibrant community of entrepreneurs, freelancers, designers, startups, small businesses and professionals – a melting pot for business from different verticals and stages. So you might just end up meeting your next business partner or picking up a new skill, find mentoring, find support for your ideas and feel inspired to reach the next level. Be a part of a rich community",
     author: "Eccosphere",
-    date: "March 2, 2025",
+    date: "May 21, 2021",
   },
   {
     id: 3,
@@ -41,7 +41,7 @@ export const blogData = [
     content:
       "For all those free-spirited mavericks tired of working from home offices and coffee shops and longing for swanky and snazzy workspaces, here it is. Uber-cool workstations, private offices, brainstorming corners, professional boardrooms, get all the flexibility, comfort and amenities that you want.",
     author: "Eccosphere",
-    date: "March 2, 2025",
+    date: "May 25, 2021",
   },
   {
     id: 4,
@@ -52,7 +52,7 @@ export const blogData = [
     content:
       "Want to pick up specific domain knowledge, get nuggets of advice from successful entrepreneurs and mentors, and network with prospective customers? We have you covered. From events to workshops to skills labs, we make sure you learn and grow with us.",
     author: "Eccosphere",
-    date: "March 2, 2025",
+    date: "June 12, 2021",
   },
   {
     id: 5,
@@ -63,7 +63,7 @@ export const blogData = [
     content:
       "Design is what defines Eccosphere, makes it a special and unique. We designed the space to allow its occupants to feel comfortable and focused. Our design is based on the Zen philosophy of minimalism and simplicity – celebrating natural materials and visual balance. The interiors are airy and bright with large ceilings and plenty of plant life. Open and connected work spaces sit along with private work clusters and cabins without cluttering the whole design flow",
     author: "Eccosphere",
-    date: "March 2, 2025",
+    date: "June 27, 2021",
   },
   {
     id: 6,
@@ -74,7 +74,7 @@ export const blogData = [
     content:
       "Amidst the monotony of the monochrome office existence, among the heaps of files, deadlines and work, stress and worry tend to overtake almost everyone’s mind. In such an environment, one cannot deny the impact and joy some colors could bring. Nature, in that sense, holds a supernatural capability to inject hues into the colorless canvas describing a typical workplace scenario. Acknowledging the need for people to park their worries from time to time in order to function more productively, we at Eccosphere bring to life, the concept of an indoor office park.",
     author: "Eccosphere",
-    date: "March 2, 2025",
+    date: "July 21, 2021",
   },
   {
     id: 7,
@@ -85,7 +85,7 @@ export const blogData = [
     content:
       "We believe that true power thrives where there is real knowledge.Our vision and idea is to collaborate with experts who design and formulate result oriented and knowledge packed courses, completely tailored to meet the business requirements of the members of our community.",
     author: "Eccosphere",
-    date: "March 2, 2025",
+    date: "October 6, 2021",
   },
   {
     id: 8,
@@ -96,7 +96,7 @@ export const blogData = [
     content:
       "Meditating in silence for a few minutes can ensure you find the right answers that unleash your true potential. The practice can empower you to be more productive and creative in your approach to work, while it strengthens your emotional intelligence. At Eccosphere, you can find the perfect abode to let aside some time and sweep out worries of the world to focus on your inner self.",
     author: "Eccosphere",
-    date: "March 2, 2025",
+    date: "December 15, 2021",
   },
   {
     id: 9,
@@ -107,7 +107,7 @@ export const blogData = [
     content:
       "We want your workplace to be your happy place. A place that helps to make you and your work family be their best version. A work haven, that takes care of your body, mind and business alike. A space that enables you to invest in a way, which allows your creativity to thrive, efficiency and innovation to boom, and your ideas to be abuzz amidst the overtly competitive ecosystem. To that vision, we invest in you.",
     author: "Eccosphere",
-    date: "March 2, 2025",
+    date: "January 25, 2022",
   },
   {
     id: 10,
@@ -118,6 +118,6 @@ export const blogData = [
     content:
       "Blending in with the beauty of the universe is sometimes essential to zone out, and tune in to find some inner peace. Doing so is essential to bounce back, recharged with higher levels of productivity. At Eccosphere, we envisage an office environment that helps you connect with your inner Zen. We have built a zone within the premises that enables you to connect with a part of your soul, where your true nature lies.",
     author: "Eccosphere",
-    date: "March 2, 2025",
+    date: "February 2, 2022",
   },
 ];
